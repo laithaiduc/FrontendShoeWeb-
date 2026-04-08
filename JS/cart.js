71fd890dcb064cef0f50ba5845c7966fa0f603ec
laithaiduc <haytrao12@gmail.com>
2026-04-08 22:55:39 +0700

    code xong products và sửa lại header

---
+++ JS/cart.js
@@ -0,0 +1,6 @@
+fetch("component/header.html") 
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+    })
+    .catch(error => console.error("Lỗi tải header:", error));
